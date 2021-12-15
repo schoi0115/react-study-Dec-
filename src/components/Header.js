30ffd7d8a779f8709e5e488c1a7bd61d2a450003
Seungil <schoi0115@gmail.com>
2021-12-15 18:51:24 -0500

    button and style

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+    // const name = 'Shawn'
+    // const x = true
+    const onClick = () => {
+        return (
+            console.log("Click")
+        )
+    }
+    return (
+        <header className='header'>
+
+            {/* <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1> - Styling medthod 1 */}
+            {/* <h1 style={headingStyle}>{title}</h1> - Styling Method 2 */}
+            <h1>{title}</h1>
+            <Button onClick={onClick} color='green' text='add' />
+            <Button color='blue' text='delete' />
+            <Button color='red' text='edit' />
+
+            {/* <h2>Hello</h2>
+            <h2>Hello {name} {x ? 'Yes' : 'No'}</h2> */}
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+// Header.propTypes ={
+//     title: PropTypes.string.isRequired,
+// } - Styling Method 2
+
+const headingStyle = {
+    color: 'red', backgroundColor: 'black'
+}
+export default Header
