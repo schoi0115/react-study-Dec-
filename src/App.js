8a4bff63b26e76d02f63f76c571839e0aaa01434
Seungil <schoi0115@gmail.com>
2021-12-15 19:23:26 -0500

    Reminder filter

--- src/App.js
+++ src/App.js
@@ -6,40 +6,58 @@ import Tasks from './components/Tasks'
 function App() {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
     },
     {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true
     },
     {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false
+      id: 3,
+      text: 'Food shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false
     }
 
-])
+  ])
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task)=> task.id !== id))
-}
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? {
+          ...task, reminder:
+            !task.reminder
+        } : task
+      )
+    )
+  }
 
 
   return (
     <div>
-      <Header 
-        // title = {1}
-      />
-      <Tasks 
-      tasks={tasks}
-      onDelete={deleteTask}
-      />
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+        />
+      ) : (
+        'No task'
+      )}
+
+
+
     </div>
   );
 }
